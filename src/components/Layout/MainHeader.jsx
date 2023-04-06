9a93985b94be649407e4964075b67809ebd01458
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-06 12:16:56 -0400

    ui slice

--- src/components/Layout/MainHeader.jsx
+++ src/components/Layout/MainHeader.jsx
@@ -1,14 +1,27 @@
 import Button from "../UI/Button";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
+
 import classes from "./MainHeader.module.scss";
 
 const MainHeader = (props) => {
+	const dispatch = useDispatch();
+
+	const toggleCartHandler = () => {
+		dispatch(uiActions.toggle());
+	};
+
 	return (
 		<header className={classes.header}>
 			<h1>ReduxCart</h1>
 			<nav>
 				<ul>
 					<li>
-						<Button title="My Cart" setBadge={true} />
+						<Button
+							title="My Cart"
+							setBadge={true}
+							clickHandler={toggleCartHandler}
+						/>
 					</li>
 				</ul>
 			</nav>
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,8 @@
 import classes from "./Button.module.scss";
 
-const Button = ({ title, setBadge }) => {
+const Button = ({ title, setBadge, clickHandler }) => {
 	return (
-		<button className={classes.button}>
+		<button className={classes.button} onClick={clickHandler}>
 			<span>{title}</span>
 			{setBadge && <span className={classes.badge}>1</span>}
 		</button>
